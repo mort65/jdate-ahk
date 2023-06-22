42f6c558fc2f59a54899e803f103a8a18f531bca
Mort65 <Mort65@protonmail.com>
2023-06-22 11:17:16 +0000

    Add files via upload

---
+++ jdate.py
@@ -0,0 +1,70 @@
+import datetime
+
+def gregorian_to_jalali(gd,gm,gy):
+    #converts gregorian date to jalali
+    if not gy or not gm or not gd:
+        raise ValueError
+    g_d_m = [ 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 ]
+    gy2 = (gy + 1) if(gm > 2) else gy    
+    days = 355666 + (365 * gy) + (((gy2 + 3) // 4)) - (((gy2 + 99) // 100)) + (((gy2 + 399) // 400)) + gd + g_d_m[gm-1]
+    jy = -1595 + (33 * ((days // 12053)))
+    days %= 12053
+    jy += (4 * ((days // 1461)))
+    days = (days%1461)
+    if (days > 365):
+        jy += ((days - 1) // 365)
+        days = ((days - 1)%365)
+    jm = (1 + (days // 31)) if (days < 186) else (7 + ((days - 186) // 30))   
+    jd = 1 + ((days%31) if (days < 186) else  ((days - 186)%30))    
+    jalali = [ jd, jm, jy ]
+    return jalali
+
+
+def jalali_to_gregorian(jd,jm,jy):
+    #converts jalali date to gregorian
+    if not jy or not jm or not jd:
+        raise ValueError
+    jy += 1595
+    days = -355668 + (365 * jy) + (( (jy // 33)) * 8) + (((jy%33) + 3) // 4) + jd + ((jm - 1) * 31 if (jm < 7) else ((jm - 7) * 30) + 186)
+    
+    gy = 400 * ((days // 146097))
+    days = (days%146097)
+    if (days > 36524):
+        gy += 100 * ((--days // 36524))
+        days = (days%36524)
+        if (days >= 365):
+            days+=1
+    gy += 4 * ((days // 1461))
+    days = (days%1461)
+    if (days > 365):
+        gy +=  ((days - 1) // 365)
+        days = ((days - 1)%365)
+    gd = days + 1
+    sal_a = [ 0, 31, 29 if (((gy%4) == 0 and (gy%100) != 0) or ((gy%400) == 0)) else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]    
+    gm = 0
+    while(gm < 13 and gd > sal_a[gm]):
+        gd -= sal_a[gm]
+        gm+=1
+    gregorian = [ gd-1, gm, gy ]
+    return gregorian
+
+def gToday():
+    gDate = datetime.datetime.today()
+    return [gDate.day, gDate.month, gDate.year]
+
+def jToday():
+    gDate = gToday()
+    return (gregorian_to_jalali(gDate[0], gDate[1], gDate[2]))
+    
+def formatDate(date):
+    if not (type(date) == list and len(date) > 2):
+        raise ValueError
+    else:
+        return "{0:0=4d}/{1:0=2d}/{2:0=2d}".format(date[2],date[1],date[0])
+
+#print(gregorian_to_jalali(22,6,2023))
+#print(jalali_to_gregorian(1,4,1402))
+#print(gToday())
+#print(jToday())
+#print(formatDate(gToday()))
+print(formatDate(jToday()))
